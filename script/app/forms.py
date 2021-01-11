2f40fe8fe5febc5a48f5ed0d5185eb33cef7ff21
Christian Melot <christianmelot1989@gmail.com>
2021-01-11 16:51:37 +0100

    progress html

--- script/app/forms.py
+++ script/app/forms.py
@@ -6,8 +6,8 @@ choices = ["bike","walk","drive"]
 weight = ["fast","safe","do you want to die?"]
 
 class Location(FlaskForm):
-    location_start = StringField(label= "From",validators=[DataRequired()])
-    location_to = StringField(label = "To",validators=[DataRequired()])
+    location_start = StringField(label= "Point de départ",validators=[DataRequired()])
+    location_to = StringField(label = "Destination",validators=[DataRequired()])
     transportation = SelectField(u"Transportation",choices=choices)
     pick = SelectField(u"Transportation",choices=weight)
     submit = SubmitField("Navigate !")
