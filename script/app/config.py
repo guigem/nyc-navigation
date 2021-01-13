ef76f6adb242276356948af608118408ffdc8750
Guillaume <gemisguillaume@gmail.com>
2021-01-13 15:41:03 +0100

    last try

--- script/app/config.py
+++ script/app/config.py
@@ -8,15 +8,15 @@ startTime = datetime.now()
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "NY-NAV-BABY"
 
-    bike_dangerous = os.path.join(basedir,"CSV\\bike_dangerous.graphml")
-    bike_safest = os.path.join(basedir,"CSV\\bike_safest.graphml")
-    bike_safest_ratio = os.path.join(basedir,"CSV\\bike_safest_ratio.graphml")
-    drive_dangerous = os.path.join(basedir,"CSV\\drive_dangerous.graphml")
-    drive_safest = os.path.join(basedir,"CSV\\drive_safest.graphml")
-    drive_safest_ratio = os.path.join(basedir,"CSV\\drive_safest_ratio.graphml")
-    walk_dangerous = os.path.join(basedir,"CSV\\walk_dangerous.graphml")
-    walk_safest = os.path.join(basedir,"CSV\\walk_safest.graphml")
-    walk_safest_ratio = os.path.join(basedir,"CSV\\walk_safest_ratio.graphml")
+    bike_dangerous = os.path.join(basedir,"CSV/bike_dangerous.graphml")
+    bike_safest = os.path.join(basedir,"CSV/bike_safest.graphml")
+    bike_safest_ratio = os.path.join(basedir,"CSV/bike_safest_ratio.graphml")
+    drive_dangerous = os.path.join(basedir,"CSV/drive_dangerous.graphml")
+    drive_safest = os.path.join(basedir,"CSV/drive_safest.graphml")
+    drive_safest_ratio = os.path.join(basedir,"CSV/drive_safest_ratio.graphml")
+    walk_dangerous = os.path.join(basedir,"CSV/walk_dangerous.graphml")
+    walk_safest = os.path.join(basedir,"CSV/walk_safest.graphml")
+    walk_safest_ratio = os.path.join(basedir,"CSV/walk_safest_ratio.graphml")
     
     '''
     print("Building Networks")
